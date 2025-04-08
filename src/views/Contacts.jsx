a7338471d69edb973b5a738a5cf39cd489f7756d
CarVill30 <carlosrogervillarreal3@gmail.com>
2025-04-08 02:04:13 +0000

    probando el Layout

--- src/views/Contacts.jsx
+++ src/views/Contacts.jsx
@@ -1,33 +1,30 @@
-import React, { useState, useEffect, useRef, useContext } from "react";
-import { Link } from 'react-router-dom'
+import React, { useContext } from "react";
+import { Link } from 'react-router-dom';
 import { Context } from "../store/appContext.js";
 import CardContact from "../component/CardContact.jsx";
 
 const Contacts = () => {
-
-    const { store, actions } = useContext(Context)
-    console.log(store.listContacts)
-
-    // useEffect(() => {
-        
-    // }, [])
+    const { store } = useContext(Context);
+    const { listContacts } = store; // Desestructuración para mayor claridad
 
     return (
-
         <div className="w-75 mx-auto">
             <div className="d-flex justify-content-end">
                 <Link to="/AddContact">
-                    <button className="btn btn-success">Add New contact</button>
+                    <button className="btn btn-success">Add New Contact</button>
                 </Link>
             </div>
             <ul className="list-group mt-3">
-                {store.listContacts && store.listContacts.length > 0 && store.listContacts.map((contact, index) => {
-                    return (
-                        <CardContact contact={contact} key={index} />
-                    )
-                })}
+                {listContacts && listContacts.length > 0 ? (
+                    listContacts.map((contact) => (
+                        <CardContact contact={contact} key={contact.id} /> // Usa un identificador único
+                    ))
+                ) : (
+                    <li className="list-group-item">No contacts available</li> // Mensaje si no hay contactos
+                )}
             </ul>
         </div>
     );
 };
+
 export default Contacts;
